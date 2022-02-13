57ba9119f362db582389e869ab9028d605cc9ab0
SajidTechnicalHub <sajid.mayoo@gmail.com>
2022-02-13 19:42:03 +0500

    Add design nikkahlab.com

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,21 @@ Rails.application.routes.draw do
   get 'profiles/index'
   get 'settings/index'
   # get 'dashboard/index'
-  # get 'admin/index'
+  root 'admin#index'
+  get 'admin/about'
+  get 'admin/pricing'
   get 'registration/index'
   devise_for :users
   # get 'checkout/index'
   # get 'checkout/show'
-  # get 'about/index'
+  # get 'about/index1'
   # resources :registration
   resources :checkout
   resources :about
   resources :price
   resources :dashboard
   resources :myprofile
-  root 'home#index'
+  get 'home/index'
   post 'checkout/create', to:'checkout#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
