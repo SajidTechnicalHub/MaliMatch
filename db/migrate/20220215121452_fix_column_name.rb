80d0e632834c90115231787a025977bede78f5ff
SajidTechnicalHub <sajid.mayoo@gmail.com>
2022-02-22 11:02:30 +0500

    remove date from fixed migration

--- db/migrate/20220215121452_fix_column_name.rb
+++ db/migrate/20220215121452_fix_column_name.rb
@@ -1,7 +1,6 @@
 class FixColumnName < ActiveRecord::Migration[5.2]
   def change
     
-    rename_column :profiles, :date, :dob
     rename_column :profiles, :educatiion, :education
     rename_column :profiles, :pratice, :practice
   end
