d523358dd5eb6ce0b9e51f8b310974f00623144f
SajidTechnicalHub <sajid.mayoo@gmail.com>
2022-05-27 17:16:34 +0500

    make some changes

--- test/controllers/addons_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AddonsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get addons_index_url
-    assert_response :success
-  end
-
-end
