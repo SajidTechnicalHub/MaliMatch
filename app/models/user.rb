964d677dbb94259a8408ba7f757a3945c8e54651
SajidTechnicalHub <sajid.mayoo@gmail.com>
2022-07-20 12:32:54 +0500

    send email in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :profile
+  has_one :profile, dependent: :destroy
   has_many :sender_requests, class_name: 'Request'
   has_many :receiver_requests, class_name: 'Request'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,10 +103,10 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  # host = 'example.com' #replace with your own url
-  # config.action_mailer.default_url_options = { host: host }
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  host = 'https://mymalimatch.herokuapp.com' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+  # host = 'localhost:3000'
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
