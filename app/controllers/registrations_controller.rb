b7f25e59ff82a651847a2653b3de2c649b42af3b
SajidTechnicalHub <sajid.mayoo@gmail.com>
2023-05-24 17:11:39 +0500

    remove after_login_path from Registration Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,16 +5,16 @@ class RegistrationsController < Devise::RegistrationsController
  protected
 
 
-  def after_sign_up_path_for(resource)
+  # def after_sign_up_path_for(resource)
 
-      # Tell the UserMailer to send a welcome email after save
-        # UserMailer.with(user: @user).welcome_email.deliver_later
+  #     # Tell the UserMailer to send a welcome email after save
+  #       # UserMailer.with(user: @user).welcome_email.deliver_later
 
-        UserMailer.with(user: @user).welcome_email.deliver_now
-         flash[:success] = "Thank you for creating new account! We'll get contact you soon!"
+  #       UserMailer.with(user: @user).welcome_email.deliver_now
+  #        flash[:success] = "Thank you for creating new account! We'll get contact you soon!"
 
-         reset_session
-      # home_membership_path
-      # new_user_session_path
-  end
+  #        reset_session
+  #     # home_membership_path
+  #     # new_user_session_path
+  # end
 end
